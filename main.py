4e8684515a02ef9a98ee25e802489a0bd67c04d9
Oleksii Maksymov <maksimov98gmy@gmail.com>
2025-10-17 18:12:16 +0300

    Add adding new pathes for log files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from typing_extensions import Self
 from dateutil import parser as date_parser
 from textual.app import App, ComposeResult
-from textual.containers import Grid
+from textual.containers import Grid, Container, VerticalScroll
 from textual.widgets import Footer, Header, Static, Label, Input, Switch, Button
 
 from datetime import datetime
@@ -16,17 +16,22 @@ dates = [
 
 class SSHSettings(Static):
     def compose(self) -> ComposeResult:
-        yield Label("SSH Settings")
         yield Label("Copy from localhost:", id="copy_from_localhost_label")
         yield Switch(id="copy_from_localhost", value=False, animate=True)
-        yield Input(placeholder="Host IP", id="host_ip", valid_empty=False)
-        yield Input(placeholder="Username", id="username", valid_empty=False)
-        yield Input(placeholder="Password", password=True, id="password", valid_empty=False)
+        with Container(id="ssh_inputs_container"):
+            yield Label("SSH Settings")
+            yield Input(placeholder="Host IP", id="host_ip", valid_empty=False)
+            yield Input(placeholder="Username", id="username", valid_empty=False)
+            yield Input(placeholder="Password", password=True, id="password", valid_empty=False)
 
+class PathField(Static):
+    def compose(self) -> ComposeResult:
+        yield Input("/var/log/messages", id="log_dirs")
 
 class LogsFetcher(App):
     BINDINGS = [
         ("d", "toggle_dark", "Toggle dark mode"),
+        ("a", "add_path", "Add path"),
         ]
 
     CSS_PATH = "logs_fetcher.tcss"
@@ -43,16 +48,32 @@ class LogsFetcher(App):
                 yield Label("Slow mode:", id="slow_mode_label")
                 yield Switch(id="slow_mode", value=False, animate=True)
                 yield Label("Directories with logs", id="log_dirs_label")
-                yield Input("/var/log/messages", id="log_dirs")
-                yield Button("Add path", id="add_path_btn", variant="success")
+                yield VerticalScroll(PathField(), id="path_fields")
+                yield Button("Add path", id="add_path", variant="success")
             yield SSHSettings(id="ssh_settings", classes="panel")
             yield Button("COPY", id="copy_btn", variant="primary")
 
-    def toggle_dark(self) -> None:
+    def action_add_path(self) -> None:
+        """An action to add a path."""
+        new_path = PathField()
+        self.query_one("#path_fields").mount(new_path)
+        new_path.scroll_visible()
+
+    def action_copy(self) -> None:
+        pass
+
+    def action_toggle_dark(self) -> None:
         """An action to toggle dark mode."""
         self.theme = (
             "textual-dark" if self.theme == "textual-light" else "textual-light"
         )
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        if event.button.id == "add_path":
+            self.action_add_path()
+        if event.button.id == "copy_btn":
+            self.action_copy()
 
 
 if __name__ == "__main__":
